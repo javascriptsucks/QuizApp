bd88068da3f40156070ac47d67ec015e23935291
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-13 23:57:57 +0000

    Start building quiz attempt page, query function to populate page

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -1,5 +1,7 @@
 const db = require('../connection');
 
+
+// GET ALL QUIZZES + NAME OF CREATOR
 const getQuizzes = () => {
 
   const queryTemplate = `
@@ -13,6 +15,8 @@ const getQuizzes = () => {
     });
 };
 
+
+// GET ALL QUIZ QUESTIONS BY QUIZ_ID + NAME OF CREATOR
 const getQuizzesQuestionsById = (id) => {
   const queryTemplate = `
     SELECT question_text as question, answer_text as answer, quizzes.title as title, users.name as creator
@@ -25,10 +29,27 @@ const getQuizzesQuestionsById = (id) => {
   const sqlParams = [id];
 
   return db.query(queryTemplate, sqlParams)
-    .then((res) => {
+    .then((data) => {
       // console.log(res.rows);
       return res.rows;
     });
+};
+
+
+const getQuizAttemptById = (quizAttemptId) => {
+  const queryTemplate = `
+    SELECT quiz_attempts.*, users.name as name, quizzes.title as quiz_title
+    FROM quiz_attempts
+    JOIN users ON user_id = users.id
+    JOIN quizzes ON quiz_id = quizzes.id
+    WHERE quiz_attempts.id = $1
+  ;
+  `;
+  const sqlParams = [quizAttemptId];
+
+  return db.query(queryTemplate, sqlParams)
+    .then()
+
 };
 
 
