543c2d6d1ff1e663239f96b49bbd306c0bdbe353
javascriptsucks <evix1988@live.cn>
2023-02-15 00:38:08 -0600

    sorry for breaking no master coding, have to fix a merge bug

--- routes/quizAttempt.js
+++ routes/quizAttempt.js
@@ -3,10 +3,24 @@ const router = express.Router();
 const quizzesQueries = require('../db/queries/quizzes');
 const quizAttemptsQueries = require('../db/queries/quiz_attempts');
 
+// /quizAttempt
+
+// GET to render quiz_attempt page with info from db query.
+router.get('/:attempt_id', (req, res) => {
+  const attemptID = req.params.attempt_id;
+
+  quizAttemptsQueries.getQuizAttemptById(attemptID)
+    .then(attempt => {
+      return res.render('quiz_attempt', { attempt });
+    });
+
+});
+
+
 // quizAttempt post create new attempt rows
 router.post('/', (req, res) => {
   const userId = req.session.user_id;
-  const quizId = req.query.quizid;
+  const quizId = req.body.quiz_id;
   let score = 0;
 
   quizzesQueries.getQuizzesQuestionsById(quizId)
@@ -19,9 +33,11 @@ router.post('/', (req, res) => {
         }
       });
 
-      const attempt = { quizId, userId, score };
+      const attempt = {quizId, userId, score};
+      console.log(attempt);
       quizAttemptsQueries.createAttempt(attempt)
         .then(response => {
+          console.log(response);
           res.redirect(`/quizAttempt/${response.id}`);
         });
     });
