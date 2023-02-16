bc87cf8ebb30230ca6a76b440553dd48f9426276
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-16 22:09:49 +0000

    Added fade effect on appropriate pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,8 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  // APPLY FADE IN EFFECT FOR ANY ELEMENTS WITH HIDDEN CLASS
+  $('.hidden').fadeIn('1000').removeClass('.hidden');
+
+})
 
